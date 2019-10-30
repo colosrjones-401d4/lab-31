fa29f3f1dc6b33fddd1d933881122682fedf145c
ColoSRJones <colosrjones@gmail.com>
2019-10-30 09:26:04 -0500

    push again

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { useEffect, useState, useReducer } from 'react';
 
-export default function App() {
+import Todo from './components/todo/todo';
+import Item from '../src/components/items';
+import Header from '../src/components/header';
+import Form from '../src/components/form'
+
+import './components/todo/todo';
+
+function App(props) {
   return (
-    <>
-      <div>My App</div>
-    </>
+    <section>
+       <Header /> 
+      <div>
+        <Todo />
+      </div>
+      <div>
+        <Item />
+      </div>
+      <div>
+        <Form />
+      </div>
+    </section>
   );
 }
+
+export default App;
---
+++ src/components/todo/todo.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { When } from '../if';
+import Modal from '../modal';
+import Header from '../header';
+import Form from '../form';
+import Item from '../items';
+import Details from '../details';
+
+import './todo.scss';
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [],
+      showDetails: false,
+      details: {},
+    };
+  }
+
+  addItem = item => {
+    this.setState(state => ({
+      todoList: [...state.todoList, item],
+    }));
+  }
+
+  deleteItem = id => {
+
+    this.setState(state => ({
+      todoList: state.todoList.filter(item => item._id !== id),
+    }));
+
+  };
+
+  saveItem = updatedItem => {
+
+    this.setState(state => ({
+      todoList: state.todoList.map(item =>
+        item._id === updatedItem._id ? updatedItem : item
+      ),
+    }));
+
+  };
+
+  toggleComplete = id => {
+    this.setState(state => ({
+      todoList: state.todoList.map(item =>
+        item._id === id ? {
+          ...item,
+          complete: !item.complete,
+        } : item
+      ),
+    }));
+  };
+
+  toggleDetails = id => {
+    this.setState(state => {
+      let item = state.todoList.find(item => item._id === id);
+      return {
+        details: item || {},
+        showDetails: !!item,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Header todoList={this.state.todoList} />
+
+        <section className="todo">
+          <Form addItem={this.addItem} />
+
+          <div>
+            <ul>
+              { this.state.todoList.map(item => (
+                <Item item={item}
+                  toggleComplete={this.toggleComplete}
+                  toggleDetails={this.toggleDetails}
+                  deleteItem={this.deleteItem}
+                  />
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <When condition={this.state.showDetails}>
+          <Modal title="To Do Item" close={this.toggleDetails}>
+            <Details
+              details={this.state.details}
+              />
+          </Modal>
+        </When>
+      </>
+    );
+  }
+}
+
+export default ToDo;
